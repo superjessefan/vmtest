d346c861066df5946ea7f0cb67e22a6eaa2c0313
superjessefan <superjessefan@gmail.com>
2017-06-03 16:45:12 +0800

    改用port:80

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ app.get('/',function(request, response){ //我們要處理URL為 "/" 的HTTP GET
     response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
     response.end('你好！'); //作出回應
 });
-server.listen(8080,'127.0.0.1',function(){
-    console.log('HTTP伺服器在 http://127.0.0.1:8080/ 上運行');
+server.listen(80,'127.0.0.1',function(){
+    console.log('HTTP伺服器在 http://127.0.0.1/ 上運行');
 });
