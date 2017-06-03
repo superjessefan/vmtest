e62efc5efe3e4fb446098cb3d9b2946c05287774
superjessefan <superjessefan@gmail.com>
2017-06-03 16:19:54 +0800

    使用express做第一個回應

---
+++ main.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+app.get('/',function(request, response){ //我們要處理URL為 "/" 的HTTP GET請求
+    response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+    response.end('你好！'); //作出回應
+});
+server.listen(8080,'127.0.0.1',function(){
+    console.log('HTTP伺服器在 http://127.0.0.1:8080/ 上運行');
+});
